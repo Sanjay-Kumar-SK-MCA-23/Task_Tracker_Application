2fceeae53aeb4745b2f6ee23586be1540796776a
Sanjay Kumar M <139735109+Sanjay-Kumar-SK-MCA-23@users.noreply.github.com>
2025-04-29 17:52:44 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateToken = (res, userId) => {
     httpOnly: true,
     secure: true,
     sameSite: 'None',
-    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
 
